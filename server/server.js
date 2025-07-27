cf7f185aade4f54665571bb6af61d2fa41839e5b
puri <puurrii01@gmail.com>
2025-07-27 22:33:54 +0545

    Server Intialized

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-// main server logic 
+// main server logic
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const port = process.env.PORT
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(port, () => {
+    console.log(`Listening to the port: ${port}`); 
+});
