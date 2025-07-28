a28dee179d18fa81f786c000d9c43f35ccbbf15d
puri <puurrii01@gmail.com>
2025-07-28 22:55:54 +0545

    Conditional Rendering

--- server/sockets/ws.js
+++ server/sockets/ws.js
@@ -1,1 +1,17 @@
 // websocket connection & logic
+
+const userSocketMap = {};
+
+io.on("connection", (socket) => {
+    socket.on("register", (userId) => {
+        userSocketMap[userId] = socket.id;
+    });
+    socket.one("disconnect", () => {
+        for(let userId in userSocketMap) {
+            if(userSocketMap[userId] === socket.id) {
+                delete userSocketMap[userId];
+                break;
+            }
+        }
+    });
+});
